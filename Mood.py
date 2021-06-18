1268eb7b270b11860b704a4aacfc96b73f70150e
bel <denoblesnobility2@gmail.com>
2021-06-18 19:19:59 +0100

    second commit, python script to hype my pm

---
+++ Mood.py
@@ -0,0 +1,13 @@
+def Datex():
+    print("$ $ $ $ $ $ $ ")
+    print("      $   ")
+    print("      $   ")
+    print("      $   ")
+    print("      $   ")
+    print("      $   ")
+    print("      $   ")
+    print(" $ $ $  ",end="\n\n")
+    print("J for Jessica!!!, Pm baako pe no size!!!")
+    print("mooooooodzzzzzzzz!!!!")
+
+Datex()
