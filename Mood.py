dccbc1016969eff7f61acf1558c6dd9af0fc04f9
DADSON PAPA KOW <papakowdadson@gmail.com>
2021-06-21 11:24:16 +0000

    me

--- Mood.py
+++ Mood.py
@@ -3,6 +3,7 @@ def Datex(status):
     print(" $ $ $  ",end="\n\n")
     print("J for Jessica!!!, Pm baako pe no size!!!")
     print("mooooooodzzzzzzzz!!!!")
+    print("the src has a charge to keep")
     
 
 Datex()
