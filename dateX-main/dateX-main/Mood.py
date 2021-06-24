8a07813d1c50f4c28f8c86e7e99c2c429540eb6f
Ahmed39-wq <86314960+Ahmed39-wq@users.noreply.github.com>
2021-06-23 17:23:28 -0700

    Hello

---
+++ dateX-main/dateX-main/Mood.py
@@ -0,0 +1,10 @@
+def Datex(status):
+    print("$ date x $ ")
+    print(" $ $ $  ",end="\n\n")
+    print("J for Jessica!!!, Pm baako pe no size!!!")
+    print("mooooooodzzzzzzzz!!!!")
+    print("Otafregya!!!!")
+    print("Simple kamakama")
+    print("greetings_earthlings_we_come_in_peace.")
+
+Datex()
---
+++ dateX-main/dateX-main/Readme.md
@@ -0,0 +1,4 @@
+link to prototype:
+https://www.figma.com/proto/rMkGKEqQR5kJ7c64KPgjiZ/Low-Fidelity-DateX?node-id=76%3A123&scaling=scale-down&page-id=0%3A1
+
+DateX is a dating app meant to randomly match people with the goal of developing personal,cordial or romantic relationships. 
