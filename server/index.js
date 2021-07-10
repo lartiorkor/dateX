4606a7251e950a2930e87cd22a1376f7f8e565df
hopedorkenoo <compounddork@gmail.com>
2021-07-10 14:18:10 +0000

    Messaging: Implement Conversation Model and Conversation routing with MongoDB

--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const conversationsRoute = require("./router/conversation")
+
+// initializing server
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+    console.log("Connected to MongoDB")
+})
+
+
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/conversations", conversationsRoute);
+
+app.listen(3000, () => {
+    console.log("Server running, on port 9000...")
+});
+
+
 console.log("Hello, world");
