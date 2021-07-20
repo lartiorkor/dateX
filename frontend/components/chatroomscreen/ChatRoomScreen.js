27771b086b30584b657d6a91e3d416438eb3fdd2
bel <denoblesnobility2@gmail.com>
2021-07-20 09:38:19 +0100

    added chatmessage,chatroomScreen,inputBox and Chatscreen to the components dir

---
+++ frontend/components/chatroomscreen/ChatRoomScreen.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Text,View,TouchableOpacity, TouchableWithoutFeedback, FlatList,ImageBackground} from 'react-native';
+import {useRoute} from '@react-navigation/native';
+import ChatMessage from '../chatmessage/ChatMessage';
+import InputBox from '../inputbox/InputBox';
+
+
+const DATA = [
+    {
+      id: 'u1',
+      content: 'First Item',
+      createdAt:'2021-07-19T08:15:00.000Z'
+    },
+    {
+      id: 'u2',
+      content: 'First Item',
+      createdAt:'2021-07-19T08:23:00.000Z'
+    },
+    {
+      id: 'u3',
+      content: 'First Item',
+      createdAt:'2021-07-19T08:20:00.000Z'
+    },];
+
+const ChatRoomScreen = ({navigation,route}) => {
+   
+        
+    return (
+        <ImageBackground style={{width:'100%', height:'100%'}} source={require('../../assets/BG.png')}>
+         <FlatList
+         data={DATA}
+         renderItem={({item})=> <ChatMessage message={item}/>}
+         inverted />
+         <InputBox/>
+         </ImageBackground>
+         
+    )
+} 
+
+export default ChatRoomScreen;
---
+++ frontend/components/inputbox/InputBox.js
@@ -0,0 +1,74 @@
+import React,{useState} from 'react'
+import {View,Text,StyleSheet, TextInput,Button} from 'react-native';
+import { styles } from 'styled-system';
+import {MaterialCommunityIcons,Fontisto,MaterialIcons} from 'react-native-vector-icons';
+ 
+ 
+
+export default function  InputBox  () {
+    const [message,setMessage] = useState('');
+    return (
+       <View style={styles2.container} >
+           <View style={styles2.main}> 
+               
+                  <TextInput placeholder='Type something' multiline style={styles2.input}
+                  value={message}
+                  onChangeText={ setMessage}/>
+                    
+           </View>
+           {!!message && <View style={styles2.buttonContainer}>
+               <Button color="green" style={styles2.button} title="Send"/>
+           </View>}
+            
+             
+       </View>
+
+    )
+};
+
+const styles2 = StyleSheet.create({
+    container: {
+ 
+     
+      flexDirection:'row',
+      alignItems:'flex-end',
+    
+   },
+   main: {
+     flex:1,
+     flexDirection:'row',
+     backgroundColor:'white',
+     padding:10,
+     margin:10,
+     borderRadius:50,
+     alignItems:'center',
+
+     
+   },
+   button:{
+        width:50,
+        height:50,
+        borderRadius:50,
+   },
+   buttonContainer:{
+       backgroundColor:'green',
+       borderRadius:50,
+       height:50,
+       width:50,
+       alignItems:'center',
+       justifyContent:'center',
+       padding:10,
+       margin:10,
+    
+       
+   },
+   input:{
+        flex:1,
+        marginHorizontal:10,
+       backgroundColor:'white',
+       padding:10,
+       alignItems:'flex-end',
+   }
+  });
+
+  
