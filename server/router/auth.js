ef7f1d9bc41a60f6bbdc8646f4f530921c7a9720
hopedorkenoo <compounddork@gmail.com>
2021-07-31 15:44:15 +0000

    Login Api

--- server/router/auth.js
+++ server/router/auth.js
@@ -59,21 +59,49 @@ router.post('/signup', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    const user = users.find(user.name = req.body.name)
-    if (user == null) {
-        return res.status(100).send('user doesnt exist')
-    }
-    try {
-        if (bcrypt.compare(req.body.password, user.password)) {
-            res.send('success')
+    const body = req.body;
+
+    const user_id = uuidv4();
+
+    const token = jwt.sign({ username: body.username, user_id: body.username }, process.env.JWT_KEY, { expiresIn: "1h" });
+    const sql = `SELECT * FROM userAuth WHERE username = '${body.username}' OR email = '${body.email}'`
+    db.query(sql, (err, rows) => {
+        if (!err) {
+            if (rows.length >= 1) {
+                bcrypt.compare(body.password, rows[0].passw, (err, result) => {
+
+                    if (err) {
+                        res.status(400).json({
+                            err: err
+                        })
+                    }
+                    if (result) {
+                        res.status(200).json({
+                            message: "Authentication Succesful",
+                            user: {
+                                username: rows[0].username,
+                                user_id: rows[0].user_id,
+
+                            },
+                            token: token,
+                        });
+                    } else {
+                        res.status(401).json({
+                            message: "Unauthorized credentials"
+                        })
+                    }
+                })
+            }
         } else {
-            res.send('this is not allowed')
+            console.log(err);
+            res.status(401).json({
+                err: "Unauthorized Credentials"
+            });
         }
+    })
 
-    } catch {
-        res.status(500).send()
 
-    }
+
 });
 
 module.exports = router;
