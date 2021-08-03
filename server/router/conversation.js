cbd4b9ff864e525c7b210fcbe896f4546ee2b026
hopedorkenoo <compounddork@gmail.com>
2021-08-03 11:00:34 +0000

    Conversatoins router

--- server/router/conversation.js
+++ server/router/conversation.js
@@ -5,27 +5,16 @@ const { v4: uuidv4 } = require('uuid');
 
 //new Conv
 router.post('/', async (req, res) => {
-    // const newConversation = new Conversation({
-    //     members: [req.body.senderId, req.body.receiverId],
-    // });
-
-    // try {
-    //     const savedConversation = await newConversation.save();
-    //     res.status(200).json(savedConversation);
-    // } catch (err) {
-    //     res.status(500).json(err);
-    // }
-
     const body = req.body;
- 
+
     const conversation_id = uuidv4();
     const sql = `INSERT INTO conversations ( conversation_id, user_one,user_two,conversation_type) VALUES(
         "${conversation_id}", "${body.user_one}","${body.user_two}","message"
     ) `
-    db.query(sql,(err,rows)=>{
-        if(!err){
+    db.query(sql, (err, rows) => {
+        if (!err) {
             res.status(200).send(rows);
-        }else{
+        } else {
             console.log(err);
             res.status(500).send(err);
         }
@@ -48,7 +37,7 @@ router.get('/:userId', async (req, res) => {
 
     } catch (err) {
 
-       res.status(500).send(err);
+        res.status(500).send(err);
     }
 })
 
