1103297e616da9f7f97375f66bc91cbed27590a3
lartiorkor <coloneltiorkor@gmail.com>
2021-08-25 13:16:15 +0000

    Settings Navigation Hierarchy with ContactUs

--- frontend/screens/SettingsStackScreen.js
+++ frontend/screens/SettingsStackScreen.js
@@ -4,6 +4,7 @@ import lightTheme from '../Theme/colors'
 import { createStackNavigator } from '@react-navigation/stack'
 import EditProfile from './EditProfile'
 import SettingsScreen from './SettingsScreen'
+import ContactUs from './ContactUs'
 
 const settingsStack = createStackNavigator();
 
@@ -15,6 +16,9 @@ const SettingsStackScreen = () => {
             }}/>
             <settingsStack.Screen name='EditProfile' component={EditProfile} options={{
                 headerShown: false
+            }}/>
+            <settingsStack.Screen name='ContactUs' component={ContactUs} options={{
+                headerShown: true
             }}/>
         </settingsStack.Navigator>
     )
